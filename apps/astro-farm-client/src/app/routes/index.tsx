5abd53270ceeb66a39a44c7a36269edd39af33d8
Ivan Kuzovkin <kuzovkinivanolegovich@gmail.com>
2025-05-08 00:13:54 +0300

    basic google analytics

--- apps/astro-farm-client/src/app/routes/index.tsx
+++ apps/astro-farm-client/src/app/routes/index.tsx
@@ -1,8 +1,15 @@
-import { Navigate, Outlet, Route, Routes } from 'react-router-dom'
+import { Navigate, Outlet, Route, Routes, useLocation } from 'react-router-dom'
 import { ShopPage, DomePage, FieldsPage, PlayerPage, WalletPage, LoginPage } from '../../pages'
 import { AuthOnlyRoute, GuestOnlyRoute } from '@astro/session-client'
 import { GameShell } from '../../layouts/game-shell/game-shell'
 import { useOnboarding } from '../../features/onboarding'
+import { useEffect } from 'react'
+
+declare global {
+    interface Window {
+        gtag?: (...args: any[]) => void
+    }
+}
 
 const GameShellWrapper = () => {
     useOnboarding()
@@ -15,6 +22,16 @@ const GameShellWrapper = () => {
 }
 
 export const AppRouter = () => {
+    const location = useLocation()
+
+    useEffect(() => {
+        if (window.gtag) {
+            window.gtag('config', 'G-B0EVK0LMTX', {
+                page_path: location.pathname + location.search,
+            })
+        }
+    }, [location])
+
     return (
         <Routes>
             <Route element={<GuestOnlyRoute to='/fields' />}>
